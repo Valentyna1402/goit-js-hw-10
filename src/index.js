bce0b0d75da0b069265c251012f8e16311c68bf9
Valentyna1402 <vriabchun@gmail.com>
2023-09-24 17:13:24 +0200

    hw-10-1

--- src/index.js
+++ src/index.js
@@ -13,63 +13,69 @@ let placeholder = document.createElement('option');
 placeholder.textContent = 'Please, select a cat';
 select.append(placeholder);
 
-
 axios.defaults.headers.common['x-api-key'] =
   'live_kf09OSiO5Mp8otyJDizuOeRNmxUrgFQO1MKv43Iw5wiNcLE0IDoxoQLB1zuQv27o';
 
-
-loaderString.classList.add('hidden');
-errorString.classList.add('hidden');
+hideEl(loaderString);
+hideEl(errorString);
 
 API.fetchBreeds()
   .then(data => {
-    if (data.length === 0) {
-      throw new Error(response.statusText);
-    }
-    data.map(el => {
-      return select.insertAdjacentHTML(
-        'beforeend',
-        `<option value="${el.id}">${el.name}</option>`
-      );
+    data.map(({ id, name }) => {
+      const markup = `<option value="${id}">${name}</option>`;
+      return select.insertAdjacentHTML('beforeend', markup);
     });
   })
   .catch(error => {
     console.log(error);
-    select.classList.add('hidden');
-    Notiflix.Notify.failure('Oops! Something went wrong!');
-    errorString.classList.remove('hidden');
+    hideEl(select);
+    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
   });
-  
 
 select.addEventListener('change', onSelectChange);
 
 function onSelectChange() {
-  placeholder.classList.add('hidden');;
-  select.classList.add('hidden');
-  loaderString.classList.remove('hidden');
+  hideEl(placeholder);
+  hideEl(select);
+  hideEl(catContainer);
+  showEl(loaderString);
 
   API.fetchCatByBreed(select.value)
     .then(data => {
+      if (data.length === 0) {
+        throw new Error(response.statusText);
+      }
       const breedInfo = data[0].breeds[0];
-      
       data
         .map(el => {
-          return (catContainer.innerHTML = `<img class="card-img" src="${el.url}" alt="${breedInfo.alt_names}" width="400px">
-  <h2 class="card-title">${breedInfo.name}</h2>
-  <p class="card-text">${breedInfo.description}</p>
-  <p class="card-text"><b>Temperament</b>: ${breedInfo.temperament}</p>`);
+          const markup = 
+      `<img class="card-img" src="${el.url}" alt="${breedInfo.alt_names}" width="360px">
+      <div class="card-text-container">
+      <h2 class="card-title">${breedInfo.name}</h2>
+      <p class="card-text">${breedInfo.description}</p>
+      <p class="card-text"><b>Temperament</b>: ${breedInfo.temperament}.</p>
+      </div>`;
+          return (catContainer.innerHTML = markup);
         })
         .join('');
+      showEl(catContainer);
     })
     .catch(error => {
-      console.log(error);  
-      catContainer.classList.add('hidden');
-      Notiflix.Notify.failure('Oops! Something went wrong!');
-      errorString.classList.remove('hidden');
+      console.log(error);
+      hideEl(catContainer);
+      Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
     })
     .finally(() => {
-      loaderString.classList.add('hidden');
-      select.classList.remove('hidden');
-      select.value = (placeholder.textContent = 'Select another cat');
+      hideEl(loaderString);
+      showEl(select);
+      select.value = placeholder.textContent = 'Select another cat';
     });
+}
+
+function hideEl(el) {
+  el.classList.add('hidden');
+}
+
+function showEl(el) {
+  el.classList.remove('hidden');
 }
